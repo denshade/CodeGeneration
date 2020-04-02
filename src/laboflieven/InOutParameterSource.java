599225fe0c141aef43ad5fdbde36758f2de4ee86
denshade <lieven.vaneeckhaute@gmail.com>
2020-04-02 16:00:39 +0200

    k

---
+++ src/laboflieven/InOutParameterSource.java
@@ -0,0 +1,7 @@
+package laboflieven;
+
+import java.util.List;
+
+public interface InOutParameterSource {
+    public List<InOutParameters> getInOutParameters(int curMaxRegisters);
+}
--- src/laboflieven/challenges/QuadraticFinder.java
+++ src/laboflieven/challenges/QuadraticFinder.java
@@ -10,7 +10,8 @@ import java.util.*;
 /**
  * Created by Lieven on 8/07/2015.
  */
-public class QuadraticFinder {
+public class QuadraticFinder implements InOutParameterSource
+{
 
     /**
      * //No solutions for 2 -> 5.
@@ -19,54 +20,9 @@ public class QuadraticFinder {
      */
     public static void mainRandomized(String[] args)
     {
-        double maxPopulationOverflow = 1.1;
-        //double popularParents = .8;
-
-        double winnerOfTheWorldWeight = 45000;
-        String winner = "";
-        for (int maxSizePopulation = 50000; maxSizePopulation < 100000; maxSizePopulation += 10000)
-        for (double curpopularParents = 0.8; curpopularParents < 0.9; curpopularParents+= 0.2)
-        for (int curMaxRegisters = 4; curMaxRegisters < 5; curMaxRegisters++)
-        {
-            List<InOutParameters> collection = getInOutParameters(curMaxRegisters);
-
-            ProgramFitnessExaminer evaluator = new ProgramFitnessExaminer(collection);
-
-            for (int curMaxInstructions = 10; curMaxInstructions < 15; curMaxInstructions++) {
-
-                RandomGeneticProgramIterator iterator = new RandomGeneticProgramIterator(evaluator, // InstructionEnum.values(), //);
-                        new InstructionEnum[]{InstructionEnum.Add, InstructionEnum.Sub, InstructionEnum.Mul, InstructionEnum.Div, InstructionEnum.Sqrt, InstructionEnum.Move, InstructionEnum.Invert},
-                        maxSizePopulation,
-                        maxPopulationOverflow,
-                        curpopularParents);
-
-                double bestInRetries = 450000;
-                ProgramResolution result = null;
-                List<Instruction> bestProgram = null;
-                for (int retries = 0; retries < 50; retries++) {
-
-                    result = iterator.iterate(curMaxRegisters, curMaxInstructions);
-
-                    if (result.weight < bestInRetries)
-                    {
-                        bestInRetries = result.weight;
-                        bestProgram = result.instructions;
-                    }
-
-                }
-
-                if (bestInRetries < winnerOfTheWorldWeight) {
-                    winnerOfTheWorldWeight = bestInRetries;
-                    winner = "#registers " + curMaxRegisters + " #maxinstructions " + curMaxInstructions  + " curpopularparents " + curpopularParents +" #maxpopulation " + maxSizePopulation + ' ' + bestProgram;
-                    System.out.println(winner + " with " + winnerOfTheWorldWeight );
-                }
-            }
-
-        }
-        System.out.println(winner + " with " + winnerOfTheWorldWeight);
-
-        //No solutions for 2 -> 5.
-        //34242100000
+        InstructionEnum[] enums = {InstructionEnum.Add, InstructionEnum.Sub, InstructionEnum.Mul, InstructionEnum.Div, InstructionEnum.Sqrt, InstructionEnum.Move, InstructionEnum.Invert};
+        List<Instruction> s = RandomGeneticProgramIterator.trySolutions(new QuadraticFinder(), enums, 1.1, 50000,100000,0.8,0.9,4,5);
+        System.out.println("Winner with " + s);
     }
 
     private static double calcQuad(double[] args)
@@ -79,7 +35,7 @@ public class QuadraticFinder {
 
     public static void main(String[] args) {//[r1 /= r2, Move r1 -> r4, r4 -= r3, Mul r4 -> r4, Invert r1, Sqrt r4, Sqrt r2, Sqrt r4, r3 -= r4, r3 /= r1, r1 += r4]
         List<Instruction> instructions = ProgramParser.parse("[r4 += r3, Mul r4 -> r1, Mul r3 -> r1, Mul r2 -> r3, r4 += r1, Sqrt r4, Invert r2, r2 += r1, Sqrt r4, r2 /= r4, r1 /= r2]"); //2.847396575786049
-        List<InOutParameters> collection = getInOutParameters(4);
+        List<InOutParameters> collection =  new QuadraticFinder().getInOutParameters(4);
 
         Register[] registers = Register.createRegisters(4, "r").toArray(new Register[0]);
         ProgramFitnessExaminer evaluator = new ProgramFitnessExaminer(collection);
@@ -89,14 +45,14 @@ public class QuadraticFinder {
     }
     public static void reverseIterator(){
         int curMaxRegisters = 4;
-        List<InOutParameters> collection = getInOutParameters(curMaxRegisters);
+        List<InOutParameters> collection = new QuadraticFinder().getInOutParameters(curMaxRegisters);
         ProgramFitnessExaminer evaluator = new ProgramFitnessExaminer(collection);
 
         ReverseProgramIterator iter = new ReverseProgramIterator(evaluator, new InstructionEnum[]{InstructionEnum.Add, InstructionEnum.Sub, InstructionEnum.Mul, InstructionEnum.Div, InstructionEnum.Sqrt, InstructionEnum.Move});
         iter.iterate(curMaxRegisters, 14);
     }
 
-    private static List<InOutParameters> getInOutParameters(int curMaxRegisters) {
+    public List<InOutParameters> getInOutParameters(int curMaxRegisters) {
         InOutParameters io = new InOutParameters();
         List<InOutParameters> collection = new ArrayList<>();
         collection.add(io.createParameter(io.fillDoubleArray(new double [] {2.0,-8.0,-24.0}, curMaxRegisters), calcQuad(new double [] {2.0,-8.0,-24.0}),1));
