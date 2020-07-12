a887ffe2538b338ba127b2d2e593de1c5e254444
denshade <lieven.vaneeckhaute@gmail.com>
2020-07-12 20:36:07 +0200

    counter.

--- src/laboflieven/ProgramEnumerator.java
+++ src/laboflieven/ProgramEnumerator.java
@@ -6,54 +6,67 @@ import java.util.List;
 
 public class ProgramEnumerator
 {
+    private final List<InstructionMark> options;
+    private long actualMarks;
+    private int nrRegisters;
+
+    public ProgramEnumerator(List<InstructionMark> options, int nrRegisters)
+    {
+        this.options = options;
+        actualMarks = 0;
+        this.nrRegisters = nrRegisters;
+        for (InstructionMark mark : options)
+        {
+
+            if (mark instanceof SingleRegisterInstruction) {
+                actualMarks += nrRegisters;
+            }
+            if (mark instanceof DualRegisterInstruction) {
+                actualMarks += nrRegisters * nrRegisters;
+            }
+        }
+    }
     public long convert(final List<InstructionMark> instructions)
     {
         long sum = 0;
-        long l = 13;
-        long index = 0;
-        for (InstructionMark instruction : instructions) {
-            if (instruction instanceof Add) {
-                index = 0;
-            }
-            if (instruction instanceof Cos) {
-                index = 1;
-            }
-            if (instruction instanceof Div) {
-                index = 2;
-            }
-            if (instruction instanceof Invert) {
-                index = 3;
-            }
-            if (instruction instanceof JumpIfRegister1Zero) {
-                index = 4;
-            }
-            if (instruction instanceof Log) {
-                index = 5;
-            }
-            if (instruction instanceof Mod) {
-                index = 6;
-            }
-            if (instruction instanceof Move) {
-                index = 7;
-            }
-            if (instruction instanceof Mul) {
-                index = 8;
-            }
-            if (instruction instanceof Nand) {
-                index = 9;
-            }
-            if (instruction instanceof Sin) {
-                index = 10;
-            }
-            if (instruction instanceof Sqrt) {
-                index = 11;
-            }
-            if (instruction instanceof Sub) {
-                index = 12;
-            }
-            sum += index*l;
-            l*=13;
+        long l = 1;
+        for (int currentInstruct = 0; currentInstruct < instructions.size(); currentInstruct++)
+        {
+            InstructionMark currentInstruction = instructions.get(currentInstruct);
+
+            sum = getIndexForInstruction(instructions, currentInstruction) * l;
+            l *= actualMarks;
         }
         return sum;
+    }
+
+    private long getIndexForInstruction(List<InstructionMark> instructions, InstructionMark currentInstruction) {
+        int k = 0;
+        for (int index = 0; index < instructions.size(); index++)
+        {
+            InstructionMark currentOption = instructions.get(index);
+            if (currentOption instanceof DualRegisterInstruction) {
+                int index1 = getRegisterIndex(((DualRegisterInstruction) currentOption).destination.name);
+                int index2 = getRegisterIndex(((DualRegisterInstruction) currentOption).source.name);
+                k += index1 + nrRegisters * index2;
+            }
+            if (currentOption instanceof SingleRegisterInstruction) {
+                int index1 = getRegisterIndex(((SingleRegisterInstruction) currentOption).destination.name);
+                k += index1;
+            }
+            if (currentOption.getClass().toGenericString().equals(currentInstruction.getClass().toGenericString()))
+            {
+                return k;
+            }
+        }
+        return k;
+    }
+
+    private int getRegisterIndex(String name) {
+        if (name.toLowerCase().equals("r1")) return 0;
+        if (name.toLowerCase().equals("r2")) return 1;
+        if (name.toLowerCase().equals("r3")) return 2;
+        if (name.toLowerCase().equals("r4")) return 3;
+        throw new RuntimeException("Unknown register " + name);
     }
 }
--- test/ProgramEnumeratorTest.java
+++ test/ProgramEnumeratorTest.java
@@ -4,6 +4,7 @@ import laboflieven.statements.Register;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,9 +12,10 @@ class ProgramEnumeratorTest {
 
     @Test
     void convert() {
-        ProgramEnumerator enumator = new ProgramEnumerator();
         Register r1 = new Register("R1");
         Register r2 = new Register("R2");
+
+        ProgramEnumerator enumator = new ProgramEnumerator(List.of(new Add(r1, r2), new Cos(r1), new Mul(r1, r2)), 2);
         long l = enumator.convert(Arrays.asList(new Add(r1, r2), new Cos(r1), new Mul(r1, r2)));//0+1*13+13*13*8
         assertEquals(l , 17745);
     }
