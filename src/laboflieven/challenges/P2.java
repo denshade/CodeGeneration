0b26867c8e0bb48a24780b12d0367a2d65c65e90
denshade <lieven.vaneeckhaute@gmail.com>
2020-07-13 22:19:31 +0200

    P2 launcher.

--- src/laboflieven/challenges/P2.java
+++ src/laboflieven/challenges/P2.java
@@ -6,6 +6,7 @@ import laboflieven.programiterators.AccRandomGeneticProgramIterator;
 import laboflieven.InOutParameters;
 import laboflieven.accinstructions.AccInstructionOpcode;
 import laboflieven.loggers.SysOutAccFitnessLogger;
+import laboflieven.programiterators.GeneralBruteForceProgramIterator;
 import laboflieven.runners.AccStatementRunner;
 
 import java.io.IOException;
@@ -19,17 +20,17 @@ public class P2 implements ProgramTemplate
      */
     public static void main(String[] args) throws IOException {
 
-        int curMaxRegisters = 4;
+        int curMaxRegisters = 2;
         List<double[]> points = new ArrayList<>();
         for (int i = 1; i < 10; i++) {
-            points.add(new double[] { i, Math.sqrt(5),0,0});
+            points.add(new double[] { i, Math.sqrt(5)});
         }
         List<InOutParameters> collection = TestCases.getTestCases(new P2(), points.toArray(new double[0][0]),curMaxRegisters);
         ProgramFitnessExaminerInterface evaluator = new ProgramFitnessExaminer(collection, new AccStatementRunner());
         evaluator.addListener(new SysOutAccFitnessLogger(10000));
-        AccRandomGeneticProgramIterator iter = new AccRandomGeneticProgramIterator(evaluator,  AccInstructionOpcode.values(), 10000,1.2,0.4);
+        GeneralBruteForceProgramIterator iter = new GeneralBruteForceProgramIterator(evaluator,  AccInstructionOpcode.values());
         long start = System.currentTimeMillis();
-        System.out.println(iter.iterate(curMaxRegisters, 15));
+        System.out.println(iter.iterate(curMaxRegisters, 10));
         //evaluator.writeAndClose();
         System.out.println(System.currentTimeMillis() - start + "ms");
 
