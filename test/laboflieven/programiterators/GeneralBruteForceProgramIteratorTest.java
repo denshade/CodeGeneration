3092bd8a6d9ddc7a0d53eafb66b9680c919a87c9
denshade <lieven.vaneeckhaute@gmail.com>
2020-10-07 20:53:00 +0200

    create test.

---
+++ test/laboflieven/programiterators/GeneralBruteForceProgramIteratorTest.java
@@ -0,0 +1,15 @@
+package laboflieven.programiterators;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeneralBruteForceProgramIteratorTest {
+
+    @Test
+    void iterate() {
+        GeneralBruteForceProgramIterator iter = new GeneralBruteForceProgramIterator();
+        iter.iterate(2, 2);
+
+    }
+}
