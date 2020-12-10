c67eab3c96166a0bc5a3c426897505e1a71e4548
denshade <lieven.vaneeckhaute@gmail.com>
2020-12-10 22:26:04 +0100

    selector finder.

--- src/laboflieven/genericsolutions/SelectorProgramFinder.java
+++ src/laboflieven/genericsolutions/SelectorProgramFinder.java
@@ -1,9 +1,15 @@
 package laboflieven.genericsolutions;
 
 import laboflieven.InstructionMark;
+import laboflieven.ProgramResolution;
+import laboflieven.TestcaseInOutParameters;
 import laboflieven.common.Configuration;
+import laboflieven.examiners.ProgramFitnessExaminer;
 import laboflieven.programiterators.GeneralBruteForceProgramIterator;
+import laboflieven.runners.AccStatementRunner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class SelectorProgramFinder
@@ -11,9 +17,26 @@ public class SelectorProgramFinder
     public List<InstructionMark> findSolutions(Configuration configuration)
     {
         var clonedConfiguration = new Configuration();
-        clonedConfiguration.getFitnessExaminer();
+        clonedConfiguration.setFitnessExaminer(new ProgramFitnessExaminer(configuration.getFitnessExaminer().getTestcases(), new AccStatementRunner()));
         var generalBruteForceProgramIterator = new GeneralBruteForceProgramIterator();
-        var programResolution = generalBruteForceProgramIterator.iterate(clonedConfiguration);
+        var programResolution = new ProgramResolution(new ArrayList<>(), 400);
+        int nrInstructs = 1;
+        while (programResolution == null || programResolution.weight > 0.001){
+            clonedConfiguration.setMaxNrInstructions(nrInstructs++);
+            programResolution = generalBruteForceProgramIterator.iterate(clonedConfiguration);
+        }
         return programResolution.instructions;
+    }
+
+    public List<TestcaseInOutParameters> adjustTestcases(List<TestcaseInOutParameters> testcases) {
+        var parameters = new ArrayList<TestcaseInOutParameters>();
+        int counter = 0;
+        for (TestcaseInOutParameters param : testcases) {
+            var scenario = new TestcaseInOutParameters();
+            scenario.expectedOutput.put("R1", (double) counter++);
+            scenario.input = new HashMap<>(param.input);
+            parameters.add(scenario);
+        }
+        return parameters;
     }
 }
--- test/laboflieven/genericsolutions/SelectorProgramFinderTest.java
+++ test/laboflieven/genericsolutions/SelectorProgramFinderTest.java
@@ -20,6 +20,16 @@ class SelectorProgramFinderTest {
         configuration.setFitnessExaminer(evaluator);
         var prog = selector.findSolutions(configuration);
         assertNotNull(prog);
+    }
+    @Test
+    void getTestCases()
+    {
+        TestcaseSource source = new XorFinder();
+        var selector = new SelectorProgramFinder();
+        var testcases = selector.adjustTestcases(source.getTestCases());
+        assertEquals(0, testcases.get(0).expectedOutput.get("R1"));
+        assertEquals(1, testcases.get(1).expectedOutput.get("R1"));
+        assertEquals(2, testcases.get(2).expectedOutput.get("R1"));
 
     }
 }
