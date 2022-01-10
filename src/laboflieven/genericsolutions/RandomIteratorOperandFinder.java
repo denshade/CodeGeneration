4531a8beecd004bcaccf110f4deee3512ec540a8
Lieven <lieven.vaneeckhaute@gmail.com>
2022-01-10 22:35:42 +0100

    OpcodeFinder.

---
+++ src/laboflieven/genericsolutions/RandomIteratorOperandFinder.java
@@ -0,0 +1,13 @@
+package laboflieven.genericsolutions;
+
+import laboflieven.common.InstructionOpcode;
+
+import java.util.List;
+
+public class RandomIteratorOperandFinder
+{
+    public List<InstructionOpcode> find()
+    {
+        return null;
+    }
+}
