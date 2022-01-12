a004ec55b8d5c395eb7b4c3bfcca3fb8502fb8b8
Lieven <lieven.vaneeckhaute@gmail.com>
2022-01-12 22:12:21 +0100

    RandomIteratorOperandFinder

--- src/laboflieven/genericsolutions/RandomIteratorOperandFinder.java
+++ src/laboflieven/genericsolutions/RandomIteratorOperandFinder.java
@@ -1,13 +1,20 @@
 package laboflieven.genericsolutions;
 
+import laboflieven.InstructionMark;
+import laboflieven.TestcaseInOutParameters;
+import laboflieven.common.Configuration;
 import laboflieven.common.InstructionOpcode;
+import laboflieven.programiterators.RandomProgramIterator;
 
 import java.util.List;
 
 public class RandomIteratorOperandFinder
 {
-    public List<InstructionOpcode> find()
+    public List<InstructionOpcode> find(List<TestcaseInOutParameters> collection, Configuration configuration)
     {
-        return null;
+        var genIterator = new RandomProgramIterator();
+        var result = genIterator.iterate(configuration);
+        var operands = result.instructions.stream().map(InstructionMark::getInstructionOpcode).toList();
+        return operands;
     }
 }
