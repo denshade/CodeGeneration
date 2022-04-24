bdeffceb608d6272070dc7384e78221537dc1268
denshade <lieven.vaneeckhaute@gmail.com>
2022-04-24 17:33:22 +0200

    Allow for skipping first line of csv.

--- test/laboflieven/challenges/TestCasesTest.java
+++ test/laboflieven/challenges/TestCasesTest.java
@@ -16,21 +16,21 @@ class TestCasesTest {
         File file = new File("example.csv");
         Files.write(file.toPath(), ("data1,data2\n" +
                 "2,4").getBytes());
-        assertNotNull(TestCases.loadFromCsvFile(file));
+        assertNotNull(TestCases.loadFromCsvFile(file, true));
         file.delete();
 
     }
     @Test
     void loadFromCsvString() {
         assertEquals(1, TestCases.loadFromCsvString("data1,data2\n" +
-                                            "2,4", 1).size());
+                                            "2,4", 1, true).size());
         TestcaseInOutParameters firstInOutParams = TestCases.loadFromCsvString("data1,data2\n" +
-                "2,4", 1).get(0);
+                "2,4", 1, true).get(0);
         assertEquals(4.0, firstInOutParams.input.get("R1"));
         assertEquals(2.0, firstInOutParams.expectedOutput.get("R1"));
         assertEquals(2.0, TestCases.loadFromCsvString("data1,data2\n" +
-                "2,4", 2).get(0).input.get("R1"));
+                "2,4", 2, true).get(0).input.get("R1"));
         assertEquals(4.0, TestCases.loadFromCsvString("data1,data2\n" +
-                "2,4", 2).get(0).expectedOutput.get("R1"));
+                "2,4", 2, true).get(0).expectedOutput.get("R1"));
     }
 }
