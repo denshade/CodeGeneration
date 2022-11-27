b8f5a2bba979e8f9a3f0929130256d43e417dba8
denshade <lieven.vaneeckhaute@gmail.com>
2022-11-27 16:36:14 +0100

    databaseloader.

--- src/laboflieven/db/DatabaseLoader.java
+++ src/laboflieven/db/DatabaseLoader.java
@@ -3,6 +3,7 @@ package laboflieven.db;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,9 +14,19 @@ public class DatabaseLoader
         var s = columns.stream().map(c -> c + " VARCHAR(255)").collect(Collectors.joining(", "));
         return "CREATE TABLE "+table + "(" + s+")";
     }
-    public void load(Connection connection, Map<String, List<String>> tableToColumns) throws SQLException {
+    public List<String> createTables(Map<String, List<String>> tableToColumns)
+    {
+        var sqlStatements = new ArrayList<String>();
+        for (var entry : tableToColumns.entrySet()) {
+            sqlStatements.add(createTable(entry.getKey(), entry.getValue()));
+        }
+        return sqlStatements;
+    }
+    public void load(Connection connection, Map<String, List<String>> tableToColumns, List<String[]> data) throws SQLException {
         Statement statement = connection.createStatement();
-        boolean success = statement.execute("CREATE TABLE A(id INTEGER, value VARCHAR(255))");
+        for (String sql : createTables(tableToColumns)){
+            boolean success = statement.execute(sql);
+        }
 
     }
 }
--- test/laboflieven/db/DatabaseLoaderTest.java
+++ test/laboflieven/db/DatabaseLoaderTest.java
@@ -3,6 +3,7 @@ package laboflieven.db;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,4 +14,12 @@ class DatabaseLoaderTest {
         String result = new DatabaseLoader().createTable("T1", List.of("A1", "A2", "A3"));
         assertEquals("CREATE TABLE T1(A1 VARCHAR(255), A2 VARCHAR(255), A3 VARCHAR(255))", result);
     }
+    @Test
+    void createTables() {
+        var map = Map.of("T1", List.of("A1", "A2", "A3"), "T2", List.of("B1", "B2", "B3"));
+        var result = new DatabaseLoader().createTables(map);
+        assertTrue(result.contains("CREATE TABLE T1(A1 VARCHAR(255), A2 VARCHAR(255), A3 VARCHAR(255))"));
+        assertTrue(result.contains("CREATE TABLE T2(B1 VARCHAR(255), B2 VARCHAR(255), B3 VARCHAR(255))"));
+    }
+
 }
