133f3ba056b252184bbe39d2c3a84f6e7b074967
Lieven <lieven.vaneeckhaute@gmail.com>
2024-03-10 22:04:54 +0100

    RandomIterator.

---
+++ src/laboflieven/instructions/logic/RandomIterator.java
@@ -0,0 +1,14 @@
+package laboflieven.instructions.logic;
+
+import java.util.List;
+
+public class RandomIterator
+{
+    public Formula iterate(List<Boolean> solutionsThatYieldTrue, List<Boolean> solutionsThatYieldFalse) {
+        while (true) {
+            //generate a random program
+            //check if this is an exact match.
+            //if exact match then return that formula.
+        }
+    }
+}
