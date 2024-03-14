c8d3f77bffc5127c0039919a1457eb2e88ec0b0d
Lieven <lieven.vaneeckhaute@gmail.com>
2024-03-14 21:59:41 +0100

    Random tree.

--- src/laboflieven/instructions/logic/RandomIterator.java
+++ src/laboflieven/instructions/logic/RandomIterator.java
@@ -6,27 +6,58 @@ import java.util.*;
 
 public class RandomIterator
 {
-    public Formula iterate(List<Boolean> solutionsThatYieldTrue, List<Boolean> solutionsThatYieldFalse) {
+    public Formula iterate(List<TemplateRegister<Boolean>> registers, List<List<Boolean>> solutionsThatYieldTrue, List<List<Boolean>> solutionsThatYieldFalse) {
         while (true) {
-            //generate a random program
-            //check if this is an exact match.
-            //if exact match then return that formula.
+            try {
+                var form = randomFormula(registers);
+                if (!checkValuesThatYieldTrue(registers, solutionsThatYieldTrue, form))continue;
+                if (!checkValuesThatYieldFalse(registers, solutionsThatYieldTrue, form))continue;
+                return form;
+            } catch (IllegalStateException ise) {
+            }
         }
     }
 
-    public static Formula randomFormula() {
+    private static boolean checkValuesThatYieldTrue(List<TemplateRegister<Boolean>> registers, List<List<Boolean>> solutionsThatYieldTrue, Formula form) {
+        for (int l = 0; l < solutionsThatYieldTrue.size(); l++) {
+            var scenario = solutionsThatYieldTrue.get(l);
+            for (int i = 0; i < scenario.size(); i++) {
+                registers.get(i).value = scenario.get(i);
+            }
+            boolean trueVals = form.evaluate();
+            if(!trueVals) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private static boolean checkValuesThatYieldFalse(List<TemplateRegister<Boolean>> registers, List<List<Boolean>> solutionsThatYieldTrue, Formula form) {
+        for (int l = 0; l < solutionsThatYieldTrue.size(); l++) {
+            var scenario = solutionsThatYieldTrue.get(l);
+            for (int i = 0; i < scenario.size(); i++) {
+                registers.get(i).value = scenario.get(i);
+            }
+            boolean falseVals = form.evaluate();
+            if(falseVals) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static Formula randomFormula(List<TemplateRegister<Boolean>> registers) {
         Queue<BooleanTreeBuilder.SymbolOrRegister> q = new ArrayDeque();
         for (int i = 0; i < 100; i++){
-            q.add(randomSymbol(2));
+            q.add(randomSymbol(registers));
         }
         var builder = new BooleanTreeBuilder();
         return builder.buildTree(q);
     }
 
-    private static BooleanTreeBuilder.SymbolOrRegister randomSymbol(int nrRegisters) {
+    private static BooleanTreeBuilder.SymbolOrRegister randomSymbol(List<TemplateRegister<Boolean>> registers) {
         var results = new ArrayList<BooleanTreeBuilder.SymbolOrRegister>();
-        for (int l = 0; l < nrRegisters; l++) {
-            results.add(new BooleanTreeBuilder.SymbolOrRegister(new TemplateRegister<>("R" + l)));
+        for (var register: registers) {
+            results.add(new BooleanTreeBuilder.SymbolOrRegister(register));
         }
         results.add(new BooleanTreeBuilder.SymbolOrRegister(BooleanTreeBuilder.Symbol.And));
         results.add(new BooleanTreeBuilder.SymbolOrRegister(BooleanTreeBuilder.Symbol.Or));
--- test/laboflieven/instructions/logic/RandomIteratorTest.java
+++ test/laboflieven/instructions/logic/RandomIteratorTest.java
@@ -1,6 +1,9 @@
 package laboflieven.instructions.logic;
 
+import laboflieven.registers.TemplateRegister;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,7 +11,21 @@ class RandomIteratorTest {
 
     @Test
     void randomTree() {
-        assertNotNull(RandomIterator.randomFormula());
+        assertNotNull(RandomIterator.randomFormula(List.of(new TemplateRegister<>("R1"))));
     }
+
+    @Test
+    void randomIterate() {
+        var i = new RandomIterator();
+        TemplateRegister<Boolean> r1 = new TemplateRegister<>("R1");
+        var registers = List.of(r1);
+        var formula = i.iterate(registers, List.of(List.of(true)), List.of(List.of(false)));
+        r1.value = true;
+        assertTrue(formula.evaluate());
+        r1.value = false;
+        assertFalse(formula.evaluate());
+
+    }
+
 
 }
