7a2c4b19d093178838066464b6934b67c3a8dc6b
denshade <lieven.vaneeckhaute@gmail.com>
2024-03-17 17:47:37 +0100

    Refactor.

--- test/laboflieven/instructions/logic/EvaluatorTest.java
+++ test/laboflieven/instructions/logic/EvaluatorTest.java
@@ -15,4 +15,27 @@ class EvaluatorTest {
         var evaluator = new Evaluator(registers, List.of(List.of(true, true)), List.of(List.of(false, false)));
         assertThrows(IllegalArgumentException.class, () -> evaluator.evaluate(new RegisterFormula(r1)));
     }
+
+    @Test
+    void evaluate_success() {
+        TemplateRegister<Boolean> r1 = new TemplateRegister<>("R1");
+        var registers = List.of(r1);
+        var evaluator = new Evaluator(registers, List.of(List.of(true)), List.of(List.of(false)));
+        assertTrue(evaluator.evaluate(new RegisterFormula(r1)));
+    }
+    @Test
+    void evaluate_fails() {
+        TemplateRegister<Boolean> r1 = new TemplateRegister<>("R1");
+        var registers = List.of(r1);
+        var evaluator = new Evaluator(registers, List.of(List.of(false)), List.of(List.of(true)));
+        assertFalse(evaluator.evaluate(new RegisterFormula(r1)));
+    }
+
+    @Test
+    void evaluate_fails_secondary() {
+        TemplateRegister<Boolean> r1 = new TemplateRegister<>("R1");
+        var registers = List.of(r1);
+        var evaluator = new Evaluator(registers, List.of(List.of(true)), List.of(List.of(true)));
+        assertFalse(evaluator.evaluate(new RegisterFormula(r1)));
+    }
 }
